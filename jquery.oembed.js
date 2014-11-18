42d38522f24ab945a6c1ff4918b8d94ba5db1a42
Stilley, Broch <Broch.Stilley@nfl.com>
2014-11-17 17:09:51 -0800

    BCN-633 better handling of Facebook embed links.

--- jquery.oembed.js
+++ jquery.oembed.js
@@ -736,17 +736,21 @@
             +'<p class="oembedall-updated-at">Last updated: '+data.data.pushed_at+'</p></div></div>';
         }
       }),
-    new $.fn.oembed.OEmbedProvider("facebook", "rich", ["facebook.com/(people/[^\\/]+/\\d+|[^\\/]+$)"], null
-    ,{templateRegex:/.*facebook.com\/(people\/[^\/]+\/(\d+).*|([^\/]+$))/,
+    new $.fn.oembed.OEmbedProvider("facebook", "rich", ["facebook.com"], null
+    ,{templateRegex:/.*\/([^\/]+)\/([^\/]+).*/,
       template: function(url){
         // adding script directly to DOM to make sure that it is loaded correctly.
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.text = '(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs); }(document, "script", "facebook-jssdk"));';
-        document.body.appendChild(script);
+        if(!$.fn.oembed.facebokScriptHasBeenAdded){
+          $('<div id="fb-root"></div>').appendTo('body');
+          var script = document.createElement('script');
+          script.type = 'text/javascript';
+          script.text = '(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";fjs.parentNode.insertBefore(js, fjs);}(document, "script", "facebook-jssdk"));';
+          document.body.appendChild(script);
+          $.fn.oembed.facebokScriptHasBeenAdded = true;
+        }
 
         // returning template with url of facebook post.
-        return '<div id="fb-root"></div><div class="fb-post" data-href="' + url + '" data-width="520"><div class="fb-xfbml-parse-ignore"><a href="' + url + '"></div></div>';
+        return '<div class="fb-post" data-href="' + url + '" data-width="520"><div class="fb-xfbml-parse-ignore"><a href="' + url + '"></div></div>';
         
       }
     }), 
